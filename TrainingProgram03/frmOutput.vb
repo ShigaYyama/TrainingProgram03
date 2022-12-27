2b206b8808e700720acc6fce6ba30367e0528201
泰斗 横山 <13.taito@gmail.com>
2022-12-27 11:27:01 +0900

    12/27 作成完了　指摘待機状態

--- TrainingProgram03/frmOutput.vb
+++ TrainingProgram03/frmOutput.vb
@@ -2,101 +2,68 @@
 
 Public Class formOutput
 
-    Dim arryGrb As GroupBox() = New GroupBox() {grbKadai1_1, grbKadai1_2, grbKadai1_3, grbKadai1_4, grbKadai1_5, grbKadai1_6, grbKadai1_7, grbKadai2_1}
 
     'アプリケーション起動時
     Private Sub formOutput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        frmGrb.grbKadai1_1.Visible = False
 
         'アプリケーションの2重起動確認
-        App.DuplicateCheck()
+        AppSys.duplicateCheck()
 
         'データベース接続確認
-        DataBase.Loading()
+        DataBase.loading()
 
         'コンボボックス1に課題名をセット
-        For Each cbo1Items In Task.ArryTask
-            cboKadai1.Items.Add(cbo1Items)
-        Next
+        Task.cbo1SelectItem()
 
-        grbKadai1_1.Visible = False
-        grbKadai1_2.Visible = False
-        grbKadai1_3.Visible = False
-        grbKadai1_4.Visible = False
-        grbKadai1_5.Visible = False
-        grbKadai1_6.Visible = False
-        grbKadai1_7.Visible = False
-        grbKadai2_1.Visible = False
-
-
-        ''グループボックスを全て非表示にする
-        'For Each grbs As GroupBox In arryGrb
-        '    grbs.Visible = False
-        'Next
+        'グループボックスを全て非表示にする
+        Groups.grbInvisible()
 
         '課題1-3,1-6のリストボックスに値を設定しておく
-        Dim addArray As String() = {"食品", "紳士服", "婦人服", "レストラン", "化粧品"}
-
-        ltbSalseFloor1_3.Items.AddRange(addArray)
-        ltbSalesFloor1_6.Items.AddRange(addArray)
+        Groups.itemsAdd3_6()
 
     End Sub
 
     'コンボボックス1変更時
     Private Sub cboKadai1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboKadai1.SelectedIndexChanged
-        '選択されたコンボボックス（cboKadai1）のシートに内にある問題の一覧を、コンボボックス（cboKadai2）に設定
 
-        cboKadai2.Items.Clear()
-        For Each cbo2Items In Task.cbo2SetItems(cboKadai1.Text)
-            cboKadai2.Items.Add(cbo2Items)
-        Next
+        '選択されたコンボボックス（cboKadai1）のシートに内にある問題の一覧を、コンボボックス（cboKadai2）に設定
+        Task.cbo2SetItems(cboKadai1.Text)
 
     End Sub
 
     'コンボボックス2変更時
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboKadai2.SelectedIndexChanged
 
-        ''グループボックスを全て非表示にする
-        grbKadai1_1.Visible = False
-        grbKadai1_2.Visible = False
-        grbKadai1_3.Visible = False
-        grbKadai1_4.Visible = False
-        grbKadai1_5.Visible = False
-        grbKadai1_6.Visible = False
-        grbKadai1_7.Visible = False
-        grbKadai2_1.Visible = False
+        'グループボックスを一度全て非表示にする
+        Groups.grbInvisible()
 
-        'コンボボックス1の値がPL/SQLで無ければグループボックスを表示する
-        If cboKadai1.Text <> "課題PL/SQL" Then
+        '適したグループボックスを表示する
+        Groups.grbOpen(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex)
 
-            Dim strGrb As Control() = Me.Controls.Find(Forms.grbOpen(Task.cboSelected(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex)), True)
-            If strGrb.Length > 0 Then
-                CType(strGrb(0), GroupBox).Visible = True
-            End If
-
-            CType(strGrb(0), GroupBox).Location = New Point(40, 170)
-
-        End If
     End Sub
 
     '出力ボタン押下時
     Private Sub btnOutput_Click(sender As Object, e As EventArgs) Handles btnOutput.Click
 
+        '課題に対応したSQL文を取り出し、フォームコントロールに対応してSQLを組み立てる
         Dim str As String = Nothing
         Dim ansQuery As String
+        'エラーをキャッチしたら、Boolean判別にて処理を中断
         If Task.queryCreate(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex, str) = False Then
             Exit Sub
         Else
             ansQuery = str
         End If
 
-        DataBase.outPuting(ansQuery)
+        'データベースに接続してエクセル出力
+        DataBase.outPut(ansQuery)
 
     End Sub
 
     '終了ボタン押下時
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
 
+        'アプリケーション終了
         Application.Exit()
 
     End Sub
@@ -105,7 +72,7 @@ Public Class formOutput
     Private Sub formOutput_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
 
         'データベースとの接続を切断
-        DataBase.Closing()
+        DataBase.closing()
 
     End Sub
 End Class
