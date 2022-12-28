7f212fa4496bd07906b4d31a12fcb70bab42f63c
ShigaYyama <13.taito@gmial.com>
2022-12-28 10:46:55 +0900

    12/28 不要なフォーム、クラスを削除 指摘内容を基に多数修正

--- TrainingProgram03/classDataBase.vb
+++ TrainingProgram03/classDataBase.vb
@@ -7,19 +7,18 @@ Public Class DataBase
     '接続先情報
     Public Shared strConnect As String = "Data Source=localhost/orclpdb; User ID=TEST_USER; Password=TEST_USER_PASS;"
 
+    Private Shared conn As OracleConnection = Nothing
+
     'データベースの起動→接続を確立
     Public Shared Sub loading()
 
         'データベース接続に接続出来たら確認メッセージを表示
         Try
 
-            Using conn As New OracleConnection(strConnect)
+            conn = New OracleConnection(strConnect)
 
-                conn.Open()
-                MessageBox.Show("データベースとの接続が確認出来ました。" & vbLf & "処理を続行します。")
-                conn.Close()
-
-            End Using
+            conn.Open()
+            MessageBox.Show("データベースとの接続が確認出来ました。" & vbLf & "処理を続行します。")
 
             '接続が確認出来ない場合はアプリケーションを閉じる
         Catch ex As Exception
@@ -32,121 +31,112 @@ Public Class DataBase
     End Sub
 
 
-    'SQLの結果をエクセル出力
+    'SQLの結果をエクセル出力(通常SQL文)
     Public Shared Sub outPut(oraObje As String)
 
-        'データベースに接続(Oracle)
-        Using conn As New OracleConnection(strConnect)
-            conn.Open()
+        'SQL文を実行
+        Using oraComm As OracleCommand = New OracleCommand(oraObje, conn)
 
-            'SQL文を実行
-            Using oraComm As OracleCommand = New OracleCommand(oraObje, conn)
+            Using oraAdap As New OracleDataAdapter(oraComm)
 
-                'PL/SQL課題の場合はプロシージャを実行
-                If oraObje = Task.ansStored Then
+                'Oracleをエクセル出力用に読み込む
+                Using oraRead As OracleDataReader = oraComm.ExecuteReader()
 
-                    'ストアドプロシージャを実行
-                    oraComm.CommandType = CommandType.StoredProcedure
-                    oraComm.CommandText = oraObje
+                    Dim dSet As DataSet = New DataSet()
+                    Dim dTable As DataTable
 
-                    oraComm.ExecuteNonQuery()
+                    'SQL出力結果をデータテーブルに出力
+                    oraAdap.Fill(dSet, "ExportDataTable")
+                    dTable = dSet.Tables("ExportDataTable")
 
-                    'ストアドプロシージャ実行後、コマンドタイプを通常に戻す
-                    oraComm.CommandType = CommandType.Text
+                    'エクセル用の変数設定
+                    Dim excelApp As New Excel.Application()
+                    Dim excelBooksOpen As Excel.Workbooks = excelApp.Workbooks
+                    Dim excelBook As Excel.Workbook = excelBooksOpen.Add()
+                    Dim oraSheet As Excel.Worksheet = CType(excelApp.Worksheets("sheet1"), Excel.Worksheet)
 
-                    'TRN_URIAGEテーブル全体をセレクトする
-                    Dim trnSql As String
-                    trnSql = "SELECT *" & vbLf
-                    trnSql &= "FROM TRN_URIAGE"
+                    Try
 
-                    oraComm.CommandText = trnSql
+                        '保存用のダイアログ表示
+                        Using dialog As New SaveFileDialog()
 
-                End If
+                            Dim ret As DialogResult
+                            dialog.Title = "出力データの保存先を選択"
+                            dialog.Filter = "EXCELファイル|*.xlsx"
+                            ret = dialog.ShowDialog()
 
-                Using oraAdap As New OracleDataAdapter(oraComm)
+                            If ret = DialogResult.OK Then
 
-                    'Oracleをエクセル出力用に読み込む
-                    Using oraRead As OracleDataReader = oraComm.ExecuteReader()
+                                Dim filePath As String = dialog.FileName
 
-                        Dim dSet As DataSet = New DataSet()
-                        Dim dTable As DataTable
+                                '出力先のエクセルのセル番号 = データテーブルの座標にセット
+                                'Forで行と列を繰り返して値を取得
+                                For x As Integer = 0 To dTable.Columns.Count - 1
+                                    oraSheet.Cells(1, 1 + x) = dTable.Columns(x).ColumnName
+                                Next
 
-                        'SQL出力結果をデータテーブルに出力
-                        oraAdap.Fill(dSet, "ExportDataTable")
-                        dTable = dSet.Tables("ExportDataTable")
-
-                        'エクセル用の変数設定
-                        Dim excelApp As New Excel.Application()
-                        Dim excelBooksOpen As Excel.Workbooks = excelApp.Workbooks
-                        Dim excelBook As Excel.Workbook = excelBooksOpen.Add()
-                        Dim oraSheet As Excel.Worksheet = CType(excelApp.Worksheets("sheet1"), Excel.Worksheet)
-
-                        Try
-
-                            '保存用のダイアログ表示
-                            Using dialog As New SaveFileDialog()
-
-                                Dim ret As DialogResult
-                                dialog.Title = "出力データの保存先を選択"
-                                dialog.Filter = "EXCELファイル|*.xlsx"
-                                ret = dialog.ShowDialog()
-
-                                If ret = DialogResult.OK Then
-
-                                    Dim filePath As String = dialog.FileName
-
-                                    '出力先のエクセルのセル番号 = データテーブルの座標にセット
-                                    'Forで行と列を繰り返して値を取得
-                                    For x As Integer = 0 To dTable.Columns.Count - 1
-                                        oraSheet.Cells(1, 1 + x) = dTable.Columns(x).ColumnName
+                                For x As Integer = 0 To dTable.Rows.Count - 1
+                                    For y As Integer = 0 To dTable.Columns.Count - 1
+                                        oraSheet.Cells(2 + x, 1 + y) = dTable.Rows(x).Item(y).ToString
                                     Next
+                                Next
 
-                                    For x As Integer = 0 To dTable.Rows.Count - 1
-                                        For y As Integer = 0 To dTable.Columns.Count - 1
-                                            oraSheet.Cells(2 + x, 1 + y) = dTable.Rows(x).Item(y).ToString
-                                        Next
-                                    Next
+                                excelBook.SaveAs(filePath)
 
-                                    excelBook.SaveAs(FilePath)
+                            Else
+                                MessageBox.Show("処理がキャンセルされました。")
+                                Exit Sub
 
-                                Else
-                                    MessageBox.Show("処理がキャンセルされました。")
-                                    Exit Sub
+                            End If
+                            MessageBox.Show("書き込みが完了しました。")
 
-                                End If
-                                MessageBox.Show("書き込みが完了しました。")
+                        End Using
 
-                            End Using
+                    Catch ex As Exception
+                        MessageBox.Show(ex.Message)
 
-                        Catch ex As Exception
-                            MessageBox.Show(ex.Message)
+                    Finally
+                        excelApp.Quit()
+                        Call System.Runtime.InteropServices.Marshal.ReleaseComObject(oraSheet)
+                        Call System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook)
+                        Call System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp)
 
-                        Finally
-                            ExcelApp.Quit()
-                            Call System.Runtime.InteropServices.Marshal.ReleaseComObject(OraSheet)
-                            Call System.Runtime.InteropServices.Marshal.ReleaseComObject(ExcelBook)
-                            Call System.Runtime.InteropServices.Marshal.ReleaseComObject(ExcelApp)
+                    End Try
 
-                        End Try
-
-                        'データベースから切断
-                        conn.Close()
-
-                    End Using
                 End Using
             End Using
         End Using
 
     End Sub
 
+
+    'ストアドプロシージャ実行のみのファンクション
+    Public Shared Sub doStored(oraObje As String)
+
+        'ストアドプロシージャを実行
+        Using oraComm As OracleCommand = New OracleCommand(oraObje, conn)
+
+            oraComm.CommandType = CommandType.StoredProcedure
+            oraComm.CommandText = oraObje
+
+            oraComm.ExecuteNonQuery()
+
+
+        End Using
+
+    End Sub
+
+
     'データベースを閉じる
     Public Shared Sub closing()
 
-        Dim conn As New OracleConnection(strConnect)
+        If conn IsNot Nothing Then
 
-        'もしデータベースが開いたままであれば、データベースを閉じる
-        If conn.State = 1 Then
-            conn.Close()
+            'もしデータベースが開いたままであれば、データベースを閉じる
+            If conn.State = 1 Then
+                conn.Close()
+            End If
+
         End If
 
         MessageBox.Show("データベースから切断して" & vbLf & "処理を終了します。")
--- TrainingProgram03/frmOutput.vb
+++ TrainingProgram03/frmOutput.vb
@@ -13,13 +13,19 @@ Public Class formOutput
         DataBase.loading()
 
         'コンボボックス1に課題名をセット
-        Task.cbo1SelectItem()
+        For Each cbo1Items In Task.arryTask
+            cboKadai1.Items.Add(cbo1Items)
+        Next
 
         'グループボックスを全て非表示にする
-        Groups.grbInvisible()
+        grbInvisible()
+
 
         '課題1-3,1-6のリストボックスに値を設定しておく
-        Groups.itemsAdd3_6()
+        Dim addArray As String() = {"食品", "紳士服", "婦人服", "レストラン", "化粧品"}
+
+        ltbSalseFloor1_3.Items.AddRange(addArray)
+        ltbSalesFloor1_6.Items.AddRange(addArray)
 
     End Sub
 
@@ -27,7 +33,14 @@ Public Class formOutput
     Private Sub cboKadai1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboKadai1.SelectedIndexChanged
 
         '選択されたコンボボックス（cboKadai1）のシートに内にある問題の一覧を、コンボボックス（cboKadai2）に設定
-        Task.cbo2SetItems(cboKadai1.Text)
+        cboKadai2.Items.Clear()
+
+        For Each cbo2Items In Task.cbo2SetItems(cboKadai1.Text)
+            cboKadai2.Items.Add(cbo2Items)
+        Next
+
+        'グループボックスを全て非表示にする
+        grbInvisible()
 
     End Sub
 
@@ -35,10 +48,10 @@ Public Class formOutput
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboKadai2.SelectedIndexChanged
 
         'グループボックスを一度全て非表示にする
-        Groups.grbInvisible()
+        grbInvisible()
 
         '適したグループボックスを表示する
-        Groups.grbOpen(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex)
+        grbOpen(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex)
 
     End Sub
 
@@ -49,7 +62,7 @@ Public Class formOutput
         Dim str As String = Nothing
         Dim ansQuery As String
         'エラーをキャッチしたら、Boolean判別にて処理を中断
-        If Task.queryCreate(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex, str) = False Then
+        If queryCreate(cboKadai1.SelectedIndex, cboKadai2.SelectedIndex, str) = False Then
             Exit Sub
         Else
             ansQuery = str
@@ -75,4 +88,311 @@ Public Class formOutput
         DataBase.closing()
 
     End Sub
+
+
+    '以下、呼び出し用のプロシージャ
+
+    'グループボックスを全て非表示
+    Private Sub grbInvisible()
+
+        grbKadai1_1.Visible = False
+        grbKadai1_2.Visible = False
+        grbKadai1_3.Visible = False
+        grbKadai1_4.Visible = False
+        grbKadai1_5.Visible = False
+        grbKadai1_6.Visible = False
+        grbKadai1_7.Visible = False
+        grbKadai2_1.Visible = False
+
+    End Sub
+
+
+    '適したグループボックスを表示
+    Public Shared Sub grbOpen(cbo1 As Integer, cbo2 As Integer)
+
+        'コンボボックス1, コンボボックス2の値で、個別に割り振られた値を取得
+        '(TaskクラスのcboSelectedを呼び出す)
+        'それぞれの回答文ごとに返す値は以下の通り
+        '回答文　課題1-1 = 0
+        '回答文　課題1-2 = 1
+        '回答文　課題1-3 = 2
+        '回答文　課題1-4 = 3
+        '回答文　課題1-5 = 4
+        '回答文　課題1-6 = 5
+        '回答文　課題1-7 = 6
+        '回答文　課題2-1 = 10
+        '回答文　課題PL/SQL-1 = 20
+        Dim grbNum As Integer = Task.cboSelected(cbo1, cbo2)
+
+        'コンボボックスの選択された値によって、返す値を条件分岐する
+        Dim strGrb As String = Nothing
+
+        Select Case grbNum
+                '回答文　課題1
+            Case 0
+                strGrb = "grbKadai1_1"
+            Case 1
+                strGrb = "grbKadai1_2"
+            Case 2
+                strGrb = "grbKadai1_3"
+            Case 3
+                strGrb = "grbKadai1_4"
+            Case 4
+                strGrb = "grbKadai1_5"
+            Case 5
+                strGrb = "grbKadai1_6"
+            Case 6
+                strGrb = "grbKadai1_7"
+                '回答文　課題2
+            Case 10
+                strGrb = "grbKadai2_1"
+
+        End Select
+
+        'コンボボックス1の値が空で無ければ、グループボックスを表示する
+        If strGrb <> "" Then
+
+            Dim searchGrb As Control() = formOutput.Controls.Find(strGrb, True)
+            If searchGrb.Length > 0 Then
+                CType(searchGrb(0), GroupBox).Visible = True
+
+                CType(searchGrb(0), GroupBox).Location = New Point(40, 170)
+            End If
+
+        End If
+
+    End Sub
+
+
+    'SQL文
+    Public Shared Function queryCreate(cbo1 As String, cbo2 As String, ByRef ansStr As String) As Boolean
+
+        queryCreate = False
+        Dim oraComm As String = Nothing
+
+        'コンボボックス1,2の値で返した数値によって条件分岐
+        Select Case Task.cboSelected(cbo1, cbo2)
+
+            '回答文　課題1-1
+            Case 0
+
+                '例外処理：売上に、数値以外を入力した場合
+                Dim i As Integer
+                If Not Integer.TryParse(formOutput.txtUriage1_1.Text, i) Then
+                    MessageBox.Show("数値を入力してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '例外処理：順序が選択されていない場合
+                If formOutput.rbtJunjoAsc1_1.Checked = False And formOutput.rbtJunjoDesc1_1.Checked = False Then
+                    MessageBox.Show("昇順又は降順を選択してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '処理内容
+                Dim strOrder As String
+                If formOutput.rbtJunjoAsc1_1.Checked = True Then
+                    strOrder = "ASC"
+                Else
+                    strOrder = "DESC"
+                End If
+
+                oraComm &= "SELECT DISTINCT 顧客マスタ.顧客番号,顧客マスタ.氏名" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "INNER JOIN 売場トラン" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = 売場トラン.顧客番号" & vbLf
+                oraComm &= "WHERE 売場トラン.売上金額>= " & formOutput.txtUriage1_1.Text & vbLf
+                oraComm &= "ORDER BY 顧客マスタ.顧客番号 " & strOrder
+
+
+            '回答文　課題1-2
+            Case 1
+
+                '例外処理：性別がいずれも選択されていない場合
+                If formOutput.rbtSexM1_2.Checked = False And formOutput.rbtSexF1_2.Checked = False And formOutput.rbtSexMF1_2.Checked = False Then
+                    MessageBox.Show("性別を選択してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '処理内容
+                Dim strGender As String
+                If formOutput.rbtSexM1_2.Checked = True Then
+                    strGender = "'男'"
+                ElseIf formOutput.rbtSexF1_2.Checked = True Then
+                    strGender = "'女'"
+                Else
+                    strGender = "'男','女'"
+                End If
+
+                oraComm &= "SELECT 顧客マスタ.性別,SUM(売場トラン.売上金額)AS 売上金額合計" & vbLf
+                oraComm &= "FROM 売場トラン" & vbLf
+                oraComm &= "INNER JOIN 顧客マスタ" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = 売場トラン.顧客番号" & vbLf
+                oraComm &= "GROUP BY 顧客マスタ.性別" & vbLf
+                oraComm &= "HAVING 顧客マスタ.性別 IN ( " & strGender & " )"
+
+
+            '回答文　課題1-3
+            Case 2
+
+                '処理内容
+                Dim strArea As String
+                strArea = formOutput.ltbSalseFloor1_3.SelectedItem
+
+                oraComm &= "SELECT 顧客マスタ.氏名" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "INNER JOIN 売場トラン" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = 売場トラン.顧客番号" & vbLf
+                oraComm &= "WHERE 売場トラン.売場 = '" & strArea & "'" & vbLf
+                oraComm &= "GROUP BY 顧客マスタ.氏名"
+
+
+            '回答文　課題1-4
+            Case 3
+
+                '例外処理：順序が選択されていない場合
+                If formOutput.rbtJunjoAsc1_4.Checked = False And formOutput.rbtJunjoDesc1_4.Checked = False Then
+                    MessageBox.Show("昇順又は降順を選択してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '処理内容
+                Dim intDay As Integer
+                intDay = CInt(formOutput.dtpBirthday1_4.Value.ToString("yyyyMMdd"))
+
+                Dim strOrder As String
+                If formOutput.rbtJunjoAsc1_1.Checked = True Then
+                    strOrder = "ASC"
+                Else
+                    strOrder = "DESC"
+                End If
+
+                oraComm &= "SELECT 氏名,TRUNC((" & intDay & " - TO_CHAR(生年月日, 'YYYYMMDD')) /10000, 0) AS 年齢" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "ORDER BY 顧客番号 " & strOrder
+
+
+            '回答文　課題1-5
+            Case 4
+
+                '例外処理：Noに、数値以外を入力した場合
+                Dim i As Integer
+                If Integer.TryParse(formOutput.txtNumberFrom1_5.Text, i) = False Or Integer.TryParse(formOutput.txtNumberTo1_5.Text, i) = False Then
+                    MessageBox.Show("数値を入力してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '例外処理：Noに、数値以外を入力した場合
+                Dim minNum As Integer = 10000
+                Dim maxNum As Integer = 99999
+                If CInt(formOutput.txtNumberFrom1_5.Text) < minNum Or CInt(formOutput.txtNumberFrom1_5.Text) > maxNum Then
+                    MessageBox.Show("数値は" & minNum & "から" & maxNum & "の間で指定してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                If CInt(formOutput.txtNumberTo1_5.Text) < minNum Or CInt(formOutput.txtNumberTo1_5.Text) > maxNum Then
+                    MessageBox.Show("数値は" & minNum & "から" & maxNum & "の間で指定してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '処理内容
+                Dim intMin, intMax As Integer
+                intMin = formOutput.txtNumberFrom1_5.Text
+                intMax = formOutput.txtNumberTo1_5.Text
+
+                oraComm &= "SELECT 氏名,連絡先１ AS 連絡先" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "WHERE 顧客番号" & vbLf
+                oraComm &= "BETWEEN '" & intMin & "' AND '" & intMax & "'" & vbLf
+                oraComm &= "UNION SELECT 氏名,連絡先２ AS 連絡先" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "WHERE 顧客番号" & vbLf
+                oraComm &= "BETWEEN '" & intMin & "' AND '" & intMax & "'"
+
+
+            '回答文　課題1-6
+            Case 5
+
+                '処理内容
+                Dim strArea As String
+                strArea = formOutput.ltbSalesFloor1_6.SelectedItem
+
+                oraComm &= "SELECT 顧客マスタ.氏名,COUNT(売場トラン.売場) AS 回数" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "LEFT JOIN 売場トラン" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = 売場トラン.顧客番号" & vbLf
+                oraComm &= "AND 売場トラン.売場 IN('" & strArea & "')" & vbLf
+                oraComm &= "GROUP BY 顧客マスタ.氏名"
+
+
+            '回答文　課題1-7
+            Case 6
+
+                '処理内容
+                Dim strName As String
+                strName = formOutput.txtSearch1_7.Text
+
+                oraComm &= "SELECT 顧客マスタ.氏名,COUNT(売場トラン.売場) AS 回数" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "LEFT JOIN 売場トラン" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = 売場トラン.顧客番号" & vbLf
+                oraComm &= "AND 売場トラン.売場 IN('レストラン')" & vbLf
+                oraComm &= "WHERE 顧客マスタ.氏名 LIKE '%" & strName & "%'" & vbLf
+                oraComm &= "GROUP BY 顧客マスタ.氏名"
+
+
+            '回答文　課題2-1
+            Case 10
+
+                '例外処理：Noに、数値以外を入力した場合
+                Dim i As Integer
+                If Integer.TryParse(formOutput.txtNumberFrom2_1.Text, i) = False Or Integer.TryParse(formOutput.txtNumberTo2_1.Text, i) = False Then
+                    MessageBox.Show("数値を入力してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '例外処理：Noに、数値以外を入力した場合
+                Dim minNum As Integer = 10000
+                Dim maxNum As Integer = 99999
+                If CInt(formOutput.txtNumberFrom2_1.Text) < minNum Or CInt(formOutput.txtNumberFrom2_1.Text) > maxNum Then
+                    MessageBox.Show("数値は" & minNum & "から" & maxNum & "の間で指定してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                If CInt(formOutput.txtNumberTo2_1.Text) < minNum Or CInt(formOutput.txtNumberTo2_1.Text) > maxNum Then
+                    MessageBox.Show("数値は" & minNum & "から" & maxNum & "の間で指定してください", "帳票出力", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return False
+                End If
+
+                '処理内容
+                Dim intMin, intMax As Integer
+                intMin = formOutput.txtNumberFrom2_1.Text
+                intMax = formOutput.txtNumberTo2_1.Text
+
+                oraComm &= "SELECT ログイン情報.顧客番号,ログイン情報.LOGIN_ID,顧客マスタ.氏名" & vbLf
+                oraComm &= "FROM 顧客マスタ" & vbLf
+                oraComm &= "LEFT JOIN ログイン情報" & vbLf
+                oraComm &= "ON 顧客マスタ.顧客番号 = ログイン情報.顧客番号" & vbLf
+                oraComm &= "WHERE ログイン情報.顧客番号" & vbLf
+                oraComm &= "BETWEEN '" & intMin & "' AND '" & intMax & "'"
+
+
+            '回答文　課題PLSQL
+            Case 20
+
+                'ストアドプロシージャ実行
+                DataBase.doStored("RPG002")
+
+                '処理内容を、セレクト文で値を取得
+                oraComm &= "SELECT *" & vbLf
+                oraComm &= "FROM TRN_URIAGE"
+
+        End Select
+
+        ansStr = oraComm
+        Return True
+
+    End Function
+
+
 End Class
